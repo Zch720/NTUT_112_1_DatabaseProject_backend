3391cb9b787ef20625a1784bff03dff793f24be4
ZhongCH <92720howard@gmail.com>
2023-10-13 00:25:08 +0800

    Updated project initial state

---
+++ Elecookies/Database/ElecookiesDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Elecookies.Database {
+    public class ElecookiesDbContext : DbContext {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=127.0.0.1;Port=5432;Database=elecookies;Username=postgres;Password=postgres;");
+    }
+}
